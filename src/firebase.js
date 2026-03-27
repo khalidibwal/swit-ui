d2b05446ae76420a68f93f2df078df6eac8c5f94
khalid <khalidibwal@gmail.com>
2026-03-27 12:59:47 +0700

    fix firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,25 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Konfigurasi Firebase Anda
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+// };
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBEWFY7qXfLywkzAOThwZ3b3OEEbANAk24",
+  authDomain: "swit-apps.firebaseapp.com",
+  projectId: "swit-apps",
+  storageBucket: "swit-apps.firebasestorage.app",
+  messagingSenderId: "405955118304",
+  appId: "1:405955118304:web:12da59036c60c836347c85",
+  measurementId: "G-YVBMBR6QZK"
 };
 
 // Initialize Firebase
